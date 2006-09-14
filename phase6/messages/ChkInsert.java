3ed78e6c7c109f38c12f5ed445084f8e78fc16f3
Michael Rogers <m.rogers@cs.ucl.ac.uk>
2006-09-14 14:13:22 +0000

    CHK inserts, sorry about the large number of commits, I accidentally added classfiles to the repository and I'm trying to fix it
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/load-balancing-sims@10467 67a373e5-eb02-0410-a15c-ee090a768436

---
+++ phase6/messages/ChkInsert.java
@@ -0,0 +1,21 @@
+package messages;
+
+public class ChkInsert extends Search
+{
+	// Start a new insert
+	public ChkInsert (int key, double location)
+	{
+		super (key, location);
+	}
+	
+	// Forward an insert
+	public ChkInsert (int id, int key, double closest, int htl)
+	{
+		super (id, key, closest, htl);
+	}
+	
+	public String toString()
+	{
+		return new String ("CHK insert (" +id+ "," +key+ "," +htl+ ")");
+	}
+}
