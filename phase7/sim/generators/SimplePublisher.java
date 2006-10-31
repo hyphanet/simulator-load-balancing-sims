3d579fddf464b867f97ab64cd80831b823dd102b
Michael Rogers <m.rogers@cs.ucl.ac.uk>
2006-10-31 14:44:44 +0000

    Wait longer before requesting keys... still hangs
    git-svn-id: file:///home/toad/git-migration/temprepository/trunk/apps/load-balancing-sims@10754 67a373e5-eb02-0410-a15c-ee090a768436

--- phase7/sim/generators/SimplePublisher.java
+++ phase7/sim/generators/SimplePublisher.java
@@ -1,5 +1,5 @@
 // A simple publisher that inserts CHKs using a Poisson process and informs
-// each reader of each key after an average of one minute
+// each reader of each key after an average of ten minutes
 
 package sim.generators;
 import sim.Event;
@@ -20,7 +20,7 @@ public class SimplePublisher implements EventTarget
 		readers = new HashSet<Node>();
 		// Schedule the first insert
 		double delay = -Math.log (Math.random()) / rate;
-		Event.schedule (this, delay, INSERT_CHK, null);
+		Event.schedule (this, delay, PUBLISH, null);
 	}
 	
 	public boolean addReader (Node n)
@@ -30,27 +30,27 @@ public class SimplePublisher implements EventTarget
 	
 	// Event callbacks
 	
-	private void insertChk()
+	private void publish()
 	{
 		// Insert a random key
 		int key = Node.locationToKey (Math.random());
 		Event.schedule (node, 0.0, Node.INSERT_CHK, key);
-		// Inform each reader after an average of one minute
+		// Inform each reader after an average of ten minutes
 		for (Node n : readers) {
-			double delay = -Math.log (Math.random()) * 60.0;
+			double delay = -Math.log (Math.random()) * 600.0;
 			Event.schedule (n, delay, Node.REQUEST_CHK, key);
 		}
 		// Schedule the next insert
 		double delay = -Math.log (Math.random()) / rate;
-		Event.schedule (this, delay, INSERT_CHK, null);
+		Event.schedule (this, delay, PUBLISH, null);
 	}
 	
 	// EventTarget interface
 	
 	public void handleEvent (int type, Object data)
 	{
-		if (type == INSERT_CHK) insertChk();
+		if (type == PUBLISH) publish();
 	}
 	
-	private final static int INSERT_CHK = 1;
+	private final static int PUBLISH = 1;
 }
